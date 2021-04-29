42cb6fe73b3279fb366c26e538537ba6e1279fe6
Lampard <45754698+JWooni@users.noreply.github.com>
2021-04-29 17:37:38 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+
+const {User} = require("./models/User");
+
+// application/x-www-form-urlenced
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jungwoon:qwer1234@boilerplate.kdzsq.mongodb.net/boilerplate?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongDB Connected...'))
   .catch(err => console.log(err))
@@ -11,8 +22,23 @@ mongoose.connect('mongodb+srv://jungwoon:qwer1234@boilerplate.kdzsq.mongodb.net/
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!~~안녕하세요 ~')
+  res.send('Hello World!~~안녕하세요 ~ 요우ㅡ요우 용우동')
 })
+
+
+app.post('/register', (req, res) => {
+  // 회원가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다.
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+      if(err) return res.json({success : false, err})
+      return res.status(200).json({
+        success : true
+      })
+    })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- models/User.js
+++ models/User.js
@@ -32,4 +32,4 @@ const userSchema = mongoose.Schema({
 
 const User = mongoose.model('User', userSchema)
 
-module.exports = {}
+module.exports = {User}
